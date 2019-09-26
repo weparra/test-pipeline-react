2a3afdc2c11d23bc28478a401f673db153500368
william escobar parra <weparra@zaizi.com>
2019-09-26 16:58:22 +0100

    pipelinetest

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,7 +36,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built with williams power of magic hahaha 
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
