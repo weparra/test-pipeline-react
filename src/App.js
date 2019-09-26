c12999185045d3735b114ba802e443005e7558e2
william escobar parra <weparra@zaizi.com>
2019-09-26 12:18:49 +0100

    test change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload hellooooooooo nurseeee.
+          Edit <code>src/App.js</code> and save to reload hellooooooooo.
         </p>
         <a
           className="App-link"
