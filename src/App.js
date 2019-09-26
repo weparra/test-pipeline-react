c5c5fefb2d0ef179d5f0549ed0ea29102a71da98
william escobar parra <weparra@zaizi.com>
2019-09-26 12:45:06 +0100

    helloo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload hellooooooooo.
+          Edit <code>src/App.js</code> and save to reload hellooooooooo  hasitha the mexican gangstaaa      XD     .
         </p>
         <a
           className="App-link"
